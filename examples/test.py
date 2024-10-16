fd77669ed0046cb127b3715163539c5cb40a0aed
Tack_Tau <taoshuo950111@gmail.com>
2024-10-16 11:14:15 -0400

    Snapshot of SHAPE-dev stable

---
+++ examples/test.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import numpy as np
+import ase.io
+
+
+atoms = ase.io.read('.'+'/'+'POSCAR')
+print("Number of atoms:", len(atoms))
+
+
+###################################################################################################
+
+from shape.calculator import SHAPE_Calculator
+from functools import reduce
+
+chem_nums = list(atoms.numbers)
+znucl_list = reduce(lambda re, x: re+[x] if x not in re else re, chem_nums, [])
+ntyp = len(znucl_list)
+znucl = znucl_list
+
+calc = SHAPE_Calculator(
+            cutoff = 5.0,
+            contract = False,
+            znucl = znucl,
+            lmax = 0,
+            nx = 200,
+            ntyp = ntyp
+            )
+
+atoms.calc = calc
+print ("fp_energy:\n", atoms.get_potential_energy())
+print ("fp_forces:\n", atoms.get_forces())
+print ("fp_stress:\n", atoms.get_stress())
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shape",
+    version="1.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        # For example:
+        # 'numpy',
+        # 'scipy',
+    ],
+    author="Zhu Research Group",
+    author_email="li.zhu@rutgers.edu",
+    description="A brief description of the shape library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/shape",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
